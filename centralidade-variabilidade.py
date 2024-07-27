a0e998df12e9244b5db45bb562d414d93fbe56b4
Wendell Santos <wendell.airam@gmail.com>
2024-07-27 11:34:51 -0300

    Medidas de centralidade e variabilidade
    Foi criado um novo arquivo de para medidas de centralidade e variabilidade

---
+++ centralidade-variabilidade.py
@@ -0,0 +1,97 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "05b36016-a9ba-41cc-881e-b8a6260b8eb5",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Medidas de centralidade e variabilidade"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b2d4563b-d1f3-4ce0-a986-d363c40d16ea",
+   "outputs": [],
+   "source": [
+    "#Importação das bibliotecas: Numpy para carregamento de planilhas e scipy(stats) para gerar estatísticas mais detalhadas\n",
+    "import numpy as np\n",
+    "from scipy import stats"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9a3c3add-9b44-457e-91cc-0479fa616aa1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#criação de variável com os dados dos jogadores, visualização da mediana e da média\n",
+    "jogadores = [40000, "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e5cc003f-8146-4648-9afe-8587952126f1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c85ed634-afae-4482-af9b-268346207e3b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "72fe6fb6-5a7d-4092-bba6-b008d3549f4f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0ede4fe8-7535-4295-bcbb-575ef4133ad2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8669ff63-25ee-4ad8-9981-00de50d6eb2a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
