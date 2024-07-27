124c530131a6a45db1f34d0a9b40075a31c38249
Wendell Santos <wendell.airam@gmail.com>
2024-07-27 12:10:33 -0300

    Adição de novo arquivo
    Distribuição normal com extensão .py adicionada

--- distribuicao-normal.py
+++ distribuicao-normal.py
@@ -2,7 +2,7 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": 3,
+   "execution_count": 1,
    "id": "6e3ad070-63fd-43db-ad04-fa134452cade",
    "metadata": {},
    "outputs": [],
@@ -12,7 +12,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 5,
+   "execution_count": null,
    "id": "96c3e4b6-1b2b-44f6-a856-ffd21ad2eeb2",
    "metadata": {},
    "outputs": [],
@@ -23,21 +23,10 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 6,
+   "execution_count": null,
    "id": "2a855884-f0f0-4dc3-b569-6a0e99d7e84f",
    "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "0.15865525393145707"
-      ]
-     },
-     "execution_count": 6,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
+   "outputs": [],
    "source": [
     "#Conjunto de objetos numa cesta, a média é 8 e o desvio padrão é 2 (sempre para ambos)\n",
     "#Qual é a probabilidade de tirar um objeto que o peso é menor que 6 kilos?\n",
@@ -46,45 +35,22 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 17,
+   "execution_count": null,
    "id": "5063e350-ba95-4af3-8220-60b62236466a",
    "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "0.15865525393145707"
-      ]
-     },
-     "execution_count": 17,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
+   "outputs": [],
    "source": [
     "#Qual a probabilidade de tirar um objeto que o peso é maior que 6 quilos?\n",
-    "#Coloque como comentário alternadamente para visualizar o resultado\n",
     "norm.sf(6, 8, 2)\n",
     "norm.cdf(6, 8, 2)"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": 19,
+   "execution_count": null,
    "id": "6c7012c4-2838-4028-a0ba-019a456615e5",
    "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "0.31731050786291415"
-      ]
-     },
-     "execution_count": 19,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
+   "outputs": [],
    "source": [
     "#Qual a probabilidade de tirar um objeto que o peso é menor que 6 quilos ou maior que 10 quilos?\n",
     "norm.cdf(6, 8, 2) + norm.sf(10, 8, 2)"
@@ -92,7 +58,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 21,
+   "execution_count": null,
    "id": "1c305325-9f13-48ce-84ab-a79a157710c0",
    "metadata": {},
    "outputs": [],
@@ -102,21 +68,10 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 23,
+   "execution_count": null,
    "id": "eb00654f-446f-4dca-b706-7eaffd508337",
    "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "-0.3413447460685429"
-      ]
-     },
-     "execution_count": 23,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
+   "outputs": [],
    "source": [
     "norm.sf(10, 8, 2) - norm.cdf(8, 8, 2)"
    ]
